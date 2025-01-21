1c7d8b17a17745facc4412629e5a8ed80ec5d0cb
patz <147352311+patzsl@users.noreply.github.com>
2025-01-20 22:11:43 -0300

    feat: adicionar integração com Bootstrap e serviço de mensagens
    - Adiciona Bootstrap como dependência do projeto.
    - Cria o serviço MessageService para gerenciar o envio de mensagens via HTTP.
    - Atualiza o componente AppComponent para incluir um formulário de envio de mensagens.
    - Modifica o template do AppComponent para incluir a estrutura de layout do Bootstrap.
    - Remove o arquivo de teste do AppComponent.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MessageService } from './services/message.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-ws';
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder, private messageService: MessageService) {
+    this.form = fb.group({
+      message: '',
+    });
+  }
+
+  submit() {
+    this.messageService.create(this.form.getRawValue()).subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 }
